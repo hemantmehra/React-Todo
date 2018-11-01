a25fd2f2396af4a48f988c179b04e7990e946a04
hemantmehra <mehra.hemant14@gmail.com>
2018-11-01 16:41:27 +0530

    Initial commit

---
+++ src/components/Todo.js
@@ -0,0 +1,123 @@
+import React from "react";
+import _ from "lodash";
+
+import './../style.css';
+
+export class Todo extends React.Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            todos: [
+                
+            ]
+        };
+    }
+    
+    createTask(text) {
+        this.state.todos.push({
+            text,
+            done: false
+        });
+        
+        this.setState({todos: this.state.todos});
+    }
+    
+    toggleTask(todo) {
+        let task = _.find(this.state.todos, todo);
+        task.done = !task.done;
+        this.setState({todos: this.state.todos});
+    }
+
+    removeTask(todo) {
+        var arr= this.state.todos;
+        var index= this.state.todos.indexOf(todo);
+        this.state.todos.splice(index, 1);
+        this.setState({todos: this.state.todos});
+    }
+    
+    render() {
+        return (
+            <div className="w3-card-2 w3-padding-large w3-round-large todobox ">
+            	
+                <NewTodo createTask={this.createTask.bind(this)} />
+                <TodoList todos={this.state.todos} toggle={this.toggleTask.bind(this)} remove={this.removeTask.bind(this)}  />
+            
+            </div>
+        );
+    }
+}
+
+class NewTodo extends React.Component {
+    create(event) {
+        event.preventDefault();
+        let text = this.refs.newTodoText.value;
+        if (text) {
+            this.props.createTask(text);
+            this.refs.newTodoText.value = '';
+        }
+    }
+    
+    render() {
+        return (
+            <form className="inputbox w3-center" onSubmit={this.create.bind(this)} >
+                <input type="text" className="addtodo" placeholder="Add New Point" ref="newTodoText"/> <button type="submit" className="addbtn" >+</button>
+            </form>
+
+        );
+    }
+}
+
+class TodoList extends React.Component {
+    render() {
+        let items = [];
+        let todos = _.sortBy(this.props.todos, 'done');
+        
+        for (let index in todos) {
+            items.push(
+               <TodoApp todo={todos[index]} key={index} toggle={this.props.toggle} remove={this.props.remove}/>
+
+            );
+        }
+        return (
+            <ul>
+                {items}
+            </ul>
+            
+        );
+    }
+}
+
+class TodoApp extends React.Component {
+    done(event) {
+        event.preventDefault();
+
+        this.props.toggle(this.props.todo);
+    }
+
+    delete(event) {
+        event.preventDefault();
+        this.props.remove(this.props.todo);
+    }
+
+    render() {
+        let todo = this.props.todo;
+        if (todo.done) {
+            return (
+                <li className="heading w3-green w3-padding w3-round-large todo w3-medium">
+                    <del >{todo.text}</del> <a href="" className="greenbtn w3-text-white w3-xlarge" onClick={this.done.bind(this)}>✓</a> <a href="" className="w3-text-red w3-xlarge" onClick={this.delete.bind(this)}>x</a>
+
+                </li>
+                
+            );
+        } else {
+            return (
+                <li className="heading w3-white w3-padding w3-round-large todo w3-medium">
+                    {todo.text} <a href="" className="greenbtn w3-text-green w3-xlarge" onClick={this.done.bind(this)}>✓</a> <a href="" className="w3-text-red w3-xlarge" onClick={this.delete.bind(this)}>x</a>
+                </li>
+
+            );
+        }
+    }
+}
+
---
+++ src/index.js
@@ -0,0 +1,46 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import App from './components/App';
+
+//ReactDOM.render(<App/>, document.getElementById('app'));
+
+import React from "react";
+import  { render } from "react-dom";
+
+import { Todo } from "./components/Todo";
+
+class App extends React.Component {
+	
+	render() {
+		let date= new Date(); 
+		let month= date.getMonth();
+		let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		
+		console.log(months[month]);
+
+		let day= date.getDay();
+		var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		console.log(days[day]);
+
+		let year= date.getFullYear();
+		console.log(year);
+
+		let dt= date.getDate();
+	
+		return (
+			<div className="w3-row">
+				<div className="w3-col l6 topdiv" >
+				<h2 className="w3-center w3-xxlarge w3-wide heading">Hello User</h2>
+				<p className="heading w3-wide w3-center">It's {days[day]}, {months[month]} {dt}, {year}</p>
+				</div>
+				
+				<div className="w3-col l5 ">
+					<Todo/>
+				</div>
+
+			</div>
+		);
+	}
+}
+
+render(<App/>, window.document.getElementById("app"));
